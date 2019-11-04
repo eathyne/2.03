9c3f3914f47fbad6677d5ce3b1b429fcb67425b7
Eathyn Edwards <eathynedwards@Earyns-MacBook-Air.local>
2019-11-04 17:09:44 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Employee {
+
+    private String firstname;
+    private String lastname;
+    private String SSN;
+    private String DOB;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
